f0387c7dc63240d60374961ee92ed1210e0a533d
BTSimon <sbarthatoth@gmail.com>
2023-09-28 18:50:11 +0200

    [NEW] Added new JS function

--- script/mainscript.js
+++ script/mainscript.js
@@ -2,6 +2,9 @@ function play_video() {
     alert("video starts playing");
 }
 
-function show_product_page() {
-    alert("Átirányítás");
+function show_product_page(product_num) {
+    var product_num = product_num;
+    var basetext = "../pages/productpage1.html";
+    var requested_page = basetext.replace("1", product_num);
+    window.location.assign(requested_page);
 }
